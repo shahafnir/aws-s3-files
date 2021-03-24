929ab39a68e97ad2a3d6665f34bb854ffc923810
Nir Shahaf <nirshahaf@Nirs-MacBook-Pro.local>
2021-03-24 15:58:40 +0200

    editing index.js

--- src/middleware/s3-handlers.js
+++ src/middleware/s3-handlers.js
@@ -3,13 +3,14 @@ const multer = require('multer')
 const multerS3 = require('multer-s3')
 
 const s3 = new AWS.S3({ region: process.env.AWS_REGION })
+const bucket = process.env.S3_BUCKET
 
 const fileStorage = multerS3({
     s3,
-    acl: 'public-read',
+    acl: 'private', // or 'public-read',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     contentDisposition: 'inline',
-    bucket: process.env.S3_BUCKET,
+    bucket,
     metadata: (req, file, cb) => {
         cb(null, { fieldName: file.fieldname })
     },
@@ -22,4 +23,41 @@ const fileStorage = multerS3({
 
 const uploadImageToS3 = multer({ storage: fileStorage }).single('image')
 
-module.exports = { uploadImageToS3 }
+const getImageFromS3 = async (req, res, next) => {
+    const Key = req.query.key
+
+    try {
+        const { Body } = await s3
+            .getObject({
+                Key,
+                Bucket: bucket,
+            })
+            .promise()
+
+        req.imageBuffer = Body
+        next()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const deleteImageFromS3 = async (req, res, next) => {
+    const Key = req.body.key
+
+    try {
+        await s3
+            .deleteObject({
+                Key,
+                Bucket: bucket,
+            })
+            .promise()
+
+        next()
+    } catch (error) {
+        res.status(404).send({
+            message: 'File not found',
+        })
+    }
+}
+
+module.exports = { uploadImageToS3, deleteImageFromS3, getImageFromS3 }
--- src/routers/imagesRouter.js
+++ src/routers/imagesRouter.js
@@ -1,23 +1,73 @@
 const express = require('express')
-const { uploadImageToS3 } = require('../middleware/s3-handlers')
+const { Readable } = require('stream')
+const {
+    uploadImageToS3,
+    deleteImageFromS3,
+    getImageFromS3,
+} = require('../middleware/s3-handlers')
+const Image = require('../models/imageModel')
 
 const router = new express.Router()
 
 router.post('/upload-image', uploadImageToS3, async (req, res) => {
-    console.log(req.file)
     if (!req.file) {
         res.status(422).send({
-            //422- file not arrived
             code: 420,
             message: 'File not uploaded',
         })
     }
 
-    res.send()
+    const image = new Image({
+        originalName: req.file.originalname,
+        storageName: req.file.key.split('/')[1],
+        bucket: process.env.S3_BUCKET,
+        region: process.env.AWS_REGION,
+        key: req.file.key,
+    })
+
+    try {
+        await image.save()
+
+        res.status(201).send(image)
+    } catch (error) {
+        res.status(400).send(error)
+    }
 })
 
-// router.get('/get-images')
+router.get('/get-images', async (req, res) => {
+    try {
+        const images = await Image.find({})
 
-// router.delete('/delete-image', uploadImageToS3)
+        if (!images) res.send([])
+
+        res.send(images)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.get('/get-image', getImageFromS3, async (req, res) => {
+    const imageName = req.query.name
+    const stream = Readable.from(req.imageBuffer)
+    res.setHeader('Content-Disposition', 'inline; filename=' + imageName)
+
+    stream.pipe(res)
+})
+
+router.delete('/delete-image', deleteImageFromS3, async (req, res) => {
+    const imageID = req.body.id
+
+    try {
+        const image = await Image.findByIdAndDelete(imageID)
+
+        if (!image) {
+            res.status(404).send()
+        }
+
+        res.send(image)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
 
 module.exports = router
